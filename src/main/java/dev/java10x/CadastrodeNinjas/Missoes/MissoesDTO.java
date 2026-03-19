9de0ec32fbcc063f9fa18d760f152fbc78818bff
JeanLucas05 <jeanlucas091410@gmail.com>
2026-03-19 10:30:21 -0300

    chore: inclui ajustes nos DTOs e implementa mapeamento manual entre entidade e DTO nos mappers de Ninja e Missoes

--- src/main/java/dev/java10x/CadastrodeNinjas/Missoes/MissoesDTO.java
+++ src/main/java/dev/java10x/CadastrodeNinjas/Missoes/MissoesDTO.java
@@ -1,4 +1,19 @@
 package dev.java10x.CadastrodeNinjas.Missoes;
 
+
+import dev.java10x.CadastrodeNinjas.Ninjas.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MissoesDTO {
+    private Long id;
+    private String nomedamissao;
+    private String dificuldade;
+    private List<NinjaModel> Ninjas;
 }
--- src/main/java/dev/java10x/CadastrodeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastrodeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,22 @@
 package dev.java10x.CadastrodeNinjas.Ninjas;
 
+import dev.java10x.CadastrodeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String rank;
+    private String imgurl;
+    private MissoesModel missoes;
+
 }
