0a695ed581ca9f57354e5b5f17220065dec37d74
JeanLucas05 <jeanlucas091410@gmail.com>
2026-07-16 22:36:47 -0300

    feat: Implementa a contenerizacao completa e a configuracao do nginx

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 # Build
+
+
+
 FROM maven:3.9.9-eclipse-temurin-21 AS build
 
 WORKDIR /app
--- src/main/java/dev/java10x/CadastrodeNinjas/config/config.java
+++ src/main/java/dev/java10x/CadastrodeNinjas/config/config.java
@@ -9,7 +9,7 @@ public class config implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
